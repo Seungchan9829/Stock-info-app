9554cb2059cf3e83b7582428231612ccc142e8c4
Kim Seung Chan <kimseungchan@KimuiMacBookAir.webpass.net>
2025-07-26 05:31:43 +0900

    포트 자동조정되게 설정

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from filter_low_di20 import filter_low_di20
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,8 @@ def home():
     return render_template('index.html', low_di20 = low_di20)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000, debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
